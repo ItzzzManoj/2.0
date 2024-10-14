ea6a0cc8b01f1fae6f3e8df9c621a4e2abdc26a6
Manoj <manoj.sp@zohocorp.com>
2024-10-14 20:51:46 +0530

    Done

--- app.js
+++ app.js
@@ -2,34 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const TitleComponent = function () {
-  return (
-    <h1 id="color-red">
-        I am a title component of normal function!
-    </h1>
-  );
-}
+  return <h1 id="color-red">I am a title component of normal function!</h1>;
+};
 
 const hundred = 100;
 
 const titleElement = (
   <div>
-    { hundred }
-    { TitleComponent() }
-    <h2>
-      I am heading 2!
-    </h2>
+    {hundred}
+    {TitleComponent()}
+
+    <h2>I am heading 2!</h2>
   </div>
 );
 
 const HeadingComponent = () => (
   <div>
     <TitleComponent />
-    {
-      titleElement
-    }
-    <h1 id="color-red">
-      I am a Heading Component
-    </h1>
+    {titleElement}
+    <h1 id="color-red">I am a Heading Component</h1>
   </div>
 );
 
